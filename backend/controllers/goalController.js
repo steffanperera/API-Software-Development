50c1e8c4d56cb96ee9a69faacb81c2a0370cfe66
Steffan Perera <steffanperera1997@gmail.com>
2022-04-22 09:52:16 +0530

    complete: crud functionality

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -21,21 +21,46 @@ const setGoal = asyncHandler(async (req, res) => {
     // express error handler
   }
 
-  res.status(200).json({ message: "set goal" })
+  const goal = await Goal.create({
+    text: req.body.text,
+  })
+
+  res.status(200).json(goal)
 })
 
 // desc:    update goal
 // route:   PUT /api/goals/:id
 // access:  private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update goal ${req.params.id}` })
+  const goal = await Goal.findById(req.params.id)
+
+  if (!goal) {
+    res.status(400)
+    throw new Error("Goal not found")
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+
+  res.status(200).json(updatedGoal)
 })
 
 // desc:    delete goals
 // route:   DELETE /api/goals/:id
 // access:  private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `delete goal ${req.params.id}` })
+  const goal = await Goal.findById(req.params.id)
+
+  if (!goal) {
+    res.status(400)
+    throw new Error("Goal not found")
+  }
+
+  await goal.remove()
+
+  res.status(200).json({ id: req.params.id })
+  // note: id for front end
 })
 
 module.exports = {
