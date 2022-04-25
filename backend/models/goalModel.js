d3f0a9ad2fc919222a93735fc5232b950291e7d5
Steffan Perera <steffanperera1997@gmail.com>
2022-04-25 16:30:23 +0530

    user

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose")
 
 const goalSchema = mongoose.Schema(
   {
+    // associate user with test goal
+    user: {
+      tepy: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     text: {
       type: String,
       required: [true, "please add a text value"],
