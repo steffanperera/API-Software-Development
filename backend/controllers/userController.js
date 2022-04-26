fe9e1ee01dcb21791ef9104ff21750f970f493d5
Steffan Perera <steffanperera1997@gmail.com>
2022-04-26 05:37:42 +0530

    user routes set up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,26 @@
+// desc:    register new user
+// route:   POST /api/users
+// access:  public
 const registerUser = (req, res) => {
   res.json({ message: "Register user" })
 }
 
+// desc:    login a user
+// route:   POST /api/users/login
+// access:  public
+const loginUser = (req, res) => {
+  res.json({ message: "login user" })
+}
+
+// desc:    get user data
+// route:   GET /api/users/me
+// access:  public
+const getUser = (req, res) => {
+  res.json({ message: "user data" })
+}
+
 module.exports = {
   registerUser,
+  loginUser,
+  getUser,
 }
