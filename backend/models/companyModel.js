f6680aca29a77686b76a783f23d4b0dc55d247c1
Steffan Perera <steffanperera1997@gmail.com>
2022-05-05 02:12:22 +0530

    company model

--- backend/models/companyModel.js
+++ backend/models/companyModel.js
@@ -4,16 +4,26 @@ const companySchema = mongoose.Schema({
   company_name: {
     type: String,
     required: [true, "please add company name!"],
+    unique: [true],
+  },
+  email: {
+    type: String,
+    required: [true, "please add email!"],
+    unique: [true],
   },
   password: {
     type: String,
     required: [true, "please add password!"],
     unique: [true],
   },
+  country: {
+    type: String,
+    required: [true, "add job country!"],
+  },
   job_field: {
     type: String,
-    required: [true, "add job field!"]
-  }
+    required: [true, "add job field!"],
+  },
 })
 
 module.exports = mongoose.model("Company", companySchema)
