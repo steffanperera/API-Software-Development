c5b2be1af73a06bd4fb578c8d9ca0a21e34e0b86
Steffan Perera <steffanperera1997@gmail.com>
2022-05-05 01:03:26 +0530

    commit

---
+++ backend/models/jobModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const jobSchema = mongoose.Schema({
+  text: {
+    type: String,
+    required: [true, "please add text!"],
+  },
+},
+)
+
+module.exports = mongoose.model("Job", jobSchema)
