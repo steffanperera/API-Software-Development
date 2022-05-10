43835734a6bc507d7ec6a19d553c8c5fd62bf1a6
Steffan Perera <steffanperera1997@gmail.com>
2022-05-10 21:59:11 +0530

    commit

--- mobile/lib/services/services.dart
+++ mobile/lib/services/services.dart
@@ -1,11 +1,10 @@
-
 import 'dart:io';
 
 _getUserApi() async {
   var httpClient = new HttpClient();
-  var uri = new Uri.https('yourserverurl.com', '');
+  var uri = new Uri.https('server.js', '../backend/server.js');
   var request = await httpClient.getUrl(uri);
   var response = await request.close();
   var responseBody = await response.transform(UTF8.decoder).join();
   return responseBody;
-} 
+}
